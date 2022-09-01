acbd0bbb624502550c4eda01815a639e1325e778
Shubhra Dubey <shubhra.dubey95@gmail.com>
2022-09-01 13:58:31 +0530

    js

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:["","RPA Developer","UI Path Developer","BOT Designer"],
+    strings:["","RPA Developer","UI Path Developer","UI Path Trainer"],
     typeSpeed:80,
     BackSpeed:60,
     loop:true
